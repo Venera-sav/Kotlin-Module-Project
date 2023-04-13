4d8c2fcb252cd2dd7a1cab29389450d444cbbc1c
Venera-sav <venera-sav@yandex.ru>
2023-04-13 13:55:23 +0400

    Commit2

--- src/main/kotlin/Menu.kt
+++ src/main/kotlin/Menu.kt
@@ -9,7 +9,13 @@ class Menu(var title: String, var options: List<String>) {
         print("Введите число: ")
         val input = readLine()
         return if (input != null && input.matches(Regex("^\\d+$"))) {
-            input.toInt()
+            val choice = input.toInt()
+            if (choice in 0 until options.size) {
+                choice
+            } else {
+                println("Некорректный ввод. Пожалуйста, введите число от 0 до ${options.size - 1}.")
+                getUserChoice()
+            }
         } else {
             println("Некорректный ввод. Пожалуйста, введите число.")
             getUserChoice()
